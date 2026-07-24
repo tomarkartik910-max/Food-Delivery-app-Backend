8c8cb12a7d0096f2e8953f07417f6561f2ea47a8
Kartik Tomar <tomarkartik910@gmail.com>
2026-07-24 18:03:44 +0530

    implement order management with order model, routes, and integrate Razorpay for payments; add image assets and uninstall stripe as it is not for inida now

---
+++ routes/orderRoute.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import authMiddleware from '../middleware/auth.js'
+
+const orderRouter = express.Router()
+
+export default orderRouter
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config.js'
 import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 //app config
 const app = express()
@@ -28,6 +29,7 @@ app.use("/api/food",foodRouter)
 app.use("/images",express.static("uploads"))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
