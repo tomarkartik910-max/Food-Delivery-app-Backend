0aa0abbfddcee2e4d8fd0753b0874f160c052568
Kartik Tomar <tomarkartik910@gmail.com>
2026-07-25 01:13:41 +0530

    implement order placement and verification with Razorpay integration

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,2 +1,78 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Razorpay from "razorpay";
+import crypto from "crypto"
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+//placing user order from frontend
+const placeOrder = async (req, res) => {
+  const frontend_url = "http://localhost:5173";
+
+  try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+      discount: req.body.discount,
+      delivery: req.body.delivery,
+    });
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+    const options = {
+      amount: req.body.amount * 100,
+      currency: "INR",
+      receipt: newOrder._id.toString(),
+    };
+
+    const razorpayOrder = await razorpay.orders.create(options);
+
+    res.json({
+      success: true,
+      order: razorpayOrder,
+      orderId: newOrder._id,
+    });
+  } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error"})
+  }
+};
+
+const verifyOrder = async (req, res) => {
+  const {
+    razorpay_order_id,
+    razorpay_payment_id,
+    razorpay_signature,
+    orderId,
+  } = req.body;
+
+  try {
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .update(body)
+      .digest("hex");
+
+    if (expectedSignature === razorpay_signature) {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+
+      res.json({ success: true });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+
+      res.json({ success: false, message: "Payment verification failed" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+
+
+export { placeOrder,verifyOrder };
