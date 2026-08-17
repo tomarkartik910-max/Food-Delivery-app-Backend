3c91928d34ba1cd8691036e3526125c66a523f9d
Kartik Tomar <tomarkartik910@gmail.com>
2026-08-18 03:41:29 +0530

    fixing backend bugs again

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import authMiddleware from "../middleware/auth.js"
-import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from '../controllers/orderController.js'
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from '../controllers/orderControllers.js'
 
 const orderRouter = express.Router();
 
